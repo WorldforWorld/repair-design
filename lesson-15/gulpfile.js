27a4461d3f4d6fab5e7a243a41335e0ac69dd68f
Yuri Kuzmichev <2091god@mail.ru>
2020-03-05 14:20:00 +0500

    feat: Add Futura Book & Demi fonts

--- lesson-15/gulpfile.js
+++ lesson-15/gulpfile.js
@@ -4,6 +4,7 @@ const concat = require('gulp-concat'); */
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 // Static server
 function bs() {
   serveSass();
@@ -13,12 +14,15 @@ function bs() {
       }
   });
   watch("./*.html").on('change', browserSync.reload);
-  watch("./sass/**/*.sass").on('change', serveSass);
+  watch("./sass/**/*.sass", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 }
 function serveSass() {
-  return src("./sass/*.sass")
+  return src("./sass/**/*.sass")
   .pipe(sass())
+  .pipe(autoprefixer({
+    cascade: false
+  }))
   .pipe(dest("./css"))
   .pipe(browserSync.stream());
 }
