464cd453fd971b36ada0de1a1a6e98e6b4bf2136
Yuri Kuzmichev <2091god@mail.ru>
2020-03-12 14:00:59 +0500

    feat: added a function to hide the block by clicking on the button and clicking outside the block. outline deleted

--- lesson-20/js/main.js
+++ lesson-20/js/main.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
+  const modalHidden = document.querySelector('.modal__dialog');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
+  const closeBtns = document.querySelector('.modal--visible');
+  
+
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   };
@@ -10,4 +14,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
   closeBtn.addEventListener('click', switchModal);
+  /* ----------------------------------------------- */
+  document.body.addEventListener('keyup', function (e) {
+    var key = e.keyCode;
+
+    if (key == 27) {
+
+      modal.classList.remove('modal--visible');
+    }
+}, false);
+  modal.addEventListener('click', function (e) {
+    const target = e.target;
+    const modalHiddens = target == modalHidden || modalHidden.contains(target);
+    if (!modalHiddens) {
+      modal.classList.remove('modal--visible');
+    }
+  }, false);
 });
+
